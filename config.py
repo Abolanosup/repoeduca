f083635d7691e180e52bee19bac8d7180d5081d0
Tabla2181 <102341979+Tabla2181@users.noreply.github.com>
2022-12-13 21:49:37 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+import ProxyCloud
+# Bot
+BOT_TOKEN = '5444581119:AAFTxQF9x0RXHsMA58rXCpCrAYQIrTEDl3A'
+TG_API_ID = 13233271
+TG_API_HASH = 'e3ce8145aa657c2a4cc5cf0f7183e476'
+TG_ADMIN  =  'Ghosthell0210'
+TG_USER = 'EL_Wizard'
+TG_USER1 = 'Stvz02'
+TG_USER2 = 'valdes_95'  
+TG_USER3 = 'gyi'
+
+
+# Database
+DB_LOCAL = False
+DB_HOST = 'sql.freedb.tech'
+DB_HOST_USERNAME = 'freedb_hiyabo'
+DB_HOST_PASSWORD = '?vj&ZrZhGu5txSW'
+DB_NAME = 'freedb_educaDb'
+
+if DB_LOCAL:
+    # Database Local
+    DB_HOST = ''
+    DB_HOST_USERNAME = ''
+    DB_HOST_PASSWORD = ''
+    DB_NAME = 'clutilprodb'
